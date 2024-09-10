ebd29a19f75279950a741ea7aabc4e1d401bf318
salyirina <salyirina@mail.ru>
2024-09-10 23:49:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print("1 st program")
+print(9 ** 0.5 * 5)
+
+print("2 and program")
+print (9.99 > 9.98 and 1000 != 1000.1)
+
+print("3 rd program")
+print(2 * 2 + 2  == 2 * (2 + 2))
+
+print("4 th program")
+number = float('123.456') # Преобразуем строку в дробное число
+print(int(123.456 * 10))  # Умножаем на 10 и преобразуем в целое
+print(1234 % 10) # Остаток от деления на 10
