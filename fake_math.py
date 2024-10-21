4f837ae4f7d97c30c43786fcc6bd4565880e25b9
salyirina <salyirina@mail.ru>
2024-10-21 22:31:05 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+#В fake_math создайте функцию divide, которая принимает два параметра first и second.
+# Функция должна возвращать результат деления first на second, но когда в second записан 0 - возвращать строку 'Ошибка'.
+
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    return first / second
---
+++ true_math.py
@@ -0,0 +1,12 @@
+# В true_math создайте функцию divide, которая принимает два параметра first и second (первый и второй).
+# Функция должна возвращать результат деления first на second, но когда в second записан 0 - возвращать бесконечность.
+# Бесконечность можно импортировать из встроенной библиотеки math (from math import inf)
+
+from math import inf
+
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    return first/second
+
