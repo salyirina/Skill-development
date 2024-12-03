a3dd95df5705a9854cf9099aaf3bcac82bef8b85
Ирина <salyirina@mail.ru>
2024-12-04 01:19:49 +0300

    Resolve merge conflicts

--- module_8/module_8_1.py
+++ module_8/module_8_1.py
@@ -1,26 +1,13 @@
-def add_everything_up(a, b):
+def add_everything_up(a: str | float | int, b: str | float | int) -> str | float:
     try:
-        # Проверяем, является ли объект, а экземпляром того же типа, что и объект b
-        # isinstance поддерживает вложенные проверки. Например, isinstance(a, (int, float))
-        if not isinstance(a, type(b)):
-            raise TypeError("a и b должны быть одного типа")
-
-        # Если a и b числа, возвращаем сумму
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return a + b
-
-        # Если a и b строки, возвращаем конкатенацию
-        if isinstance(a, str) and isinstance(b, str):
-            return a + b
-
-        # Для других типов данных
-        raise TypeError("Недопустимые типы данных")
-
+        # Попытка сложить два значения
+        return a + b
     except TypeError:
-        # Возвращаем строковое представление, если типы разные
+        # Если возникает TypeError, возвращаем строковое представление двух данных
         return f"{a}{b}"
 
 
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+# Тестирование
+print(add_everything_up(123.456, 'строка'))  # 123.456строка
+print(add_everything_up('яблоко', 4215))     # яблоко4215
+print(add_everything_up(123.456, 7))         # 130.456
